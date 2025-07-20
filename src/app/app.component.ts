ca6d44308093a00818a867b230e0b972cc1f7743
Álvaro <alvarofuenteg@gmail.com>
2025-07-20 15:56:34 +0200

    Por fin bien configurado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,12 @@
-import { Component} from '@angular/core';
-import { AppModule } from './app.module';
-import { HeaderComponent } from './components/header/header.component';
-import { LocationSectionComponent } from './components/location-section/location-section.component';
-import { ReviewsSectionComponent } from './components/reviews-section/reviews-section.component';
-import { CarouselSectionComponent } from './components/carousel-section/carousel-section.component';
-import { FeaturedCutsSectionComponent } from './components/featured-cuts-section/featured-cuts-section.component';
-import { PriceListSectionComponent } from './components/price-list-section/price-list-section.component';
-import { ProductsSectionComponent } from './components/products-section/products-section.component';
-import { BookingSectionComponent } from './components/booking-section/booking-section.component';
-import { USPSectionComponent } from './components/usp-section/usp-section.component';
-import { AboutSectionComponent } from './components/about-section/about-section.component';
-import { FAQSectionComponent } from './components/faq-section/faq-section.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, LocationSectionComponent, ReviewsSectionComponent, CarouselSectionComponent,
-            FeaturedCutsSectionComponent, PriceListSectionComponent, ProductsSectionComponent,
-            BookingSectionComponent, USPSectionComponent, AboutSectionComponent, FAQSectionComponent,
-            FooterComponent
-  ],
-  template: `
-    <app-header></app-header>
-    <app-location-section></app-location-section>
-    <app-reviews-section></app-reviews-section>
-    <app-carousel-section></app-carousel-section>
-    <app-featured-cuts-section></app-featured-cuts-section>
-    <app-price-list-section></app-price-list-section>
-    <app-products-section></app-products-section>
-    <app-booking-section></app-booking-section>
-    <app-usp-section></app-usp-section>
-    <app-about-section></app-about-section>
-    <app-faq-section></app-faq-section>
-    <app-footer></app-footer>
-  `,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {}
-
+export class AppComponent {
+  title = 'Daviweb';
+}
--- src/app/components/footer/footer.component.ts
+++
@@ -1,8 +0,0 @@
-// src/app/components/footer/footer.component.ts
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-})
-export class FooterComponent {}
