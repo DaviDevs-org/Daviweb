6e87b44ed5caeede85c2c6a59c9496db074f5f98
Álvaro <alvarofuenteg@gmail.com>
2025-07-25 23:16:46 +0200

    Citas refactorizado (falta funcionalidad) y añadido enlaces para las secciones

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ViewportScroller } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject, Input } from "@angular/core";
 @Component({
   selector: "app-footer",
   templateUrl: "./footer.component.html",
   styleUrls: ["./footer.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {}
+export class FooterComponent {
+  private viewportScroller = inject(ViewportScroller);
+
+  scrollToSection(elementId: string) {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ViewportScroller } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject, Input } from "@angular/core";
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private viewportScroller = inject(ViewportScroller);
+
+  scrollToSection(elementId: string) {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
+}
